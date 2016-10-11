490e03a475e2e4ca219e1f85b181ad261e1f2003
Piotr Oleksy <piotr.olek@o2.pl>
2016-10-11 23:58:05 +0200

    adding tests for 'max size' method

--- test/test_max_rec_size.py
+++ test/test_max_rec_size.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.MaxRecSize import area
+from src.MaxRecSize import area, max_size
 
 
 class TestArea(unittest.TestCase):
@@ -12,6 +12,17 @@ class TestArea(unittest.TestCase):
 
     def test_area_3(self):
         self.assertEqual(area([1]), 1)
+
+
+class TestMaxSize(unittest.TestCase):
+    def test_max_size_1(self):
+        self.assertEqual(max_size([[1, 1, 1], [0, 0, 0], [1, 1, 1]]), [(1, 3), 1, 0, 2])
+
+    def test_max_size_2(self):
+        self.assertEqual(max_size([[0, 0, 0], [1, 1, 1], [1, 1, 1]]), [(1, 3), 0, 0, -1])
+
+    def test_max_size_3(self):
+        self.assertEqual(max_size([[0, 0], [1, 1]]), [(1, 2), 0, 0, -1])
 
 
 if __name__ == '__main__':
