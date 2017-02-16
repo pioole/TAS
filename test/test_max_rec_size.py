a81c290081960265cb838fbf4d13a1f0eacf513c
Piotr Oleksy <piotr.olek@o2.pl>
2017-02-16 21:46:44 +0100

    found a bug and adding test for it

--- test/test_max_rec_size.py
+++ test/test_max_rec_size.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.MaxRecSize import max_size, max_rectangle_size, Rectangle, Point
+from src.MaxRecSize import max_size, max_rectangle_size, Rectangle, Point, find_biggest_rectangle
 
 
 class TestMaxSize(unittest.TestCase):
@@ -23,6 +23,12 @@ class TestMaxSize(unittest.TestCase):
 class TestMaxRectangleSize(unittest.TestCase):
     def test_max_rectangle_size_1(self):
         self.assertEqual(max_rectangle_size([1, 2, 6, 6, 3, 2], 0), Rectangle(top_left_point=Point(x=2, y=0), height=6, width=2))
+
+
+class TestFindBiggestRectangle(unittest.TestCase):
+    def test_find_biggest_rectangle_1(self):
+        matrix = [[9, 0, 0, 0, 0, 19, 19, 19, 19, 20, 31, 31, 31, 31, 0, 34, 34, 37, 46, 46, 46, 53]]
+        self.assertEqual(find_biggest_rectangle(matrix), Rectangle(top_left_point=Point(x=1, y=0), height=1, width=4))
 
 
 if __name__ == '__main__':
