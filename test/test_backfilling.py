a700d4bddba83a803d4bf07707c872addb412d86
Piotr Oleksy <piotr.olek@o2.pl>
2017-06-18 15:52:10 +0200

    backfill tests

--- test/test_backfilling.py
+++ test/test_backfilling.py
@@ -68,6 +68,89 @@ class TestBackfilling(unittest.TestCase):
 
         self.assertTrue(b_job not in self.cluster.running_jobs)
 
+    def test_backfilling_depth2(self):
+        self.cluster.backfill_depth = 2
+
+        job_1 = Job(1, 45, 1, 1, self.timer, self.cluster)
+        b_job = Job(2, 15, 2, 1, self.timer, self.cluster)
+        b_job2 = Job(3, 15, 2, 1, self.timer, self.cluster)
+        job_3 = Job(4, 15, 1, 1, self.timer, self.cluster)
+        job_4 = Job(5, 15, 1, 1, self.timer, self.cluster)
+        job_5 = Job(6, 15, 1, 1, self.timer, self.cluster)
+
+        job_list = [
+            job_1,
+            b_job,
+            b_job2,
+            job_3,
+            job_4,
+            job_5,
+            Job(7, 1, 1, 1, self.timer, self.cluster),
+            Job(8, 1, 1, 1, self.timer, self.cluster),
+            Job(9, 1, 1, 1, self.timer, self.cluster),
+            Job(10, 1, 1, 1, self.timer, self.cluster),
+            Job(11, 1, 1, 1, self.timer, self.cluster),
+            Job(12, 1, 1, 1, self.timer, self.cluster),
+            Job(13, 1, 1, 1, self.timer, self.cluster),
+            Job(14, 1, 1, 1, self.timer, self.cluster),
+            Job(15, 1, 1, 1, self.timer, self.cluster),
+            Job(16, 1, 1, 1, self.timer, self.cluster),
+            Job(17, 1, 1, 1, self.timer, self.cluster),
+            Job(18, 1, 1, 1, self.timer, self.cluster),
+        ]
+        self.cluster.update_job_queue(job_list)
+
+        self.cluster.run_time_tick()  # 0 - 15
+
+        self.assertTrue(b_job in self.cluster.backfill_jobs)
+        self.assertTrue(b_job not in self.cluster.running_jobs)
+        self.assertTrue(b_job2 in self.cluster.backfill_jobs)
+        self.assertTrue(b_job2 not in self.cluster.running_jobs)
+        self.assertTrue(job_1 in self.cluster.running_jobs)
+        self.assertTrue(job_3 in self.cluster.running_jobs)
+        self.assertTrue(job_4 not in self.cluster.running_jobs)
+        self.assertTrue(job_5 not in self.cluster.running_jobs)
+
+        self.cluster.run_time_tick()  # 15 - 30
+
+        self.assertTrue(b_job in self.cluster.backfill_jobs)
+        self.assertTrue(b_job not in self.cluster.running_jobs)
+        self.assertTrue(b_job2 in self.cluster.backfill_jobs)
+        self.assertTrue(b_job2 not in self.cluster.running_jobs)
+        self.assertTrue(job_1 in self.cluster.running_jobs)
+        self.assertTrue(job_3 not in self.cluster.running_jobs)
+        self.assertTrue(job_4 in self.cluster.running_jobs)
+        self.assertTrue(job_5 not in self.cluster.running_jobs)
+
+        self.cluster.run_time_tick()  # 30 - 45
+
+        self.assertTrue(b_job in self.cluster.backfill_jobs)
+        self.assertTrue(b_job not in self.cluster.running_jobs)
+        self.assertTrue(b_job2 in self.cluster.backfill_jobs)
+        self.assertTrue(b_job2 not in self.cluster.running_jobs)
+        self.assertTrue(job_1 in self.cluster.running_jobs)
+        self.assertTrue(job_4 not in self.cluster.running_jobs)
+        self.assertTrue(job_5 in self.cluster.running_jobs)
+
+        self.cluster.run_time_tick()  # 45 - 60
+
+        self.assertTrue(b_job not in self.cluster.backfill_jobs)
+        self.assertTrue(b_job in self.cluster.running_jobs)
+        self.assertTrue(b_job2 in self.cluster.backfill_jobs)
+        self.assertTrue(b_job2 not in self.cluster.running_jobs)
+        self.assertTrue(job_1 not in self.cluster.running_jobs)
+        self.assertTrue(job_5 not in self.cluster.running_jobs)
+
+        self.cluster.run_time_tick()  # 60 - 75
+
+        self.assertTrue(b_job not in self.cluster.running_jobs)
+        self.assertTrue(b_job2 not in self.cluster.backfill_jobs)
+        self.assertTrue(b_job2 in self.cluster.running_jobs)
+
+        self.cluster.run_time_tick()  # 75 - 90
+
+        self.assertTrue(b_job2 not in self.cluster.running_jobs)
+
 
 if __name__ == '__main__':
     unittest.main()
